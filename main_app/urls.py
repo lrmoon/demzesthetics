dc3d95df4f28df3c7a2c0bd14cab3d51cfc170e1
lrmoon <larariad@rocketmail.com>
2024-02-04 18:23:10 -0500

    team member

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index2, name="index2"),
     path("2/", views.index, name='index'),
-    path("", views.index2, name="index2")
+    path("team/", views.team, name="team"),
+
 ]
