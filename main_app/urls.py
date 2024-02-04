a41b51f4d88434fa9e79f14b467399f597242241
lrmoon <larariad@rocketmail.com>
2024-02-03 22:52:00 -0500

    added images

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path('2/', views.index2, name="index2")
+    path("2/", views.index, name='index'),
+    path("", views.index2, name="index2")
 ]
