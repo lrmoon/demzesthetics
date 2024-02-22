ed33ee36b51af6d972d3a6229593ce372d677c15
lrmoon <larariad@rocketmail.com>
2024-02-22 13:04:30 -0500

    updated image and testimonies

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index2, name="index"),
+    path('', views.index2, name="index"),
     path("demi/", views.demi, name="demi"),
     path("yaz/", views.yaz, name="yaz"),
 
